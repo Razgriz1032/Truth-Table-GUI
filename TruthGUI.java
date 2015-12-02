12f23fef3de352995f4d39345c2f1b6246d54396
crgold <crgold@asu.edu>
2015-12-02 02:54:38 -0700

    Update TruthGUI.java

--- TruthGUI.java
+++ TruthGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +9,7 @@ public class TruthGUI implements ActionListener, Runnable
 {
 	JFrame frame = null;
 	JPanel main = null;
+	JPanel tablePanel = null;
 	JScrollPane variablePane = null;
 	JScrollPane resultPane = null;
 	JTable variableTable = null;  //columns will be "A", "B", etc.
@@ -16,8 +19,8 @@ public class TruthGUI implements ActionListener, Runnable
 	JMenuItem addVar = null;
 	JMenuItem removeVar = null;
 	Font text = null;
-	
 	int variables = 2;
+	int maxVars = 4;
 	
 	private void createGUI()  //sets up the frame when the program is run
 	{
@@ -25,7 +28,7 @@ public class TruthGUI implements ActionListener, Runnable
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setJMenuBar(menubar);
 		frame.add(main);
-		frame.setPreferredSize(new Dimension(750, 500));
+		frame.setPreferredSize(new Dimension(762, 300));
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
@@ -38,13 +41,14 @@ public class TruthGUI implements ActionListener, Runnable
 		main = new JPanel(new GridLayout(1, 2));
 		
 		//these lines are just initializers for testing
-		variableTable = new JTable(8, 2);
-		resultTable = new JTable(8, 10);
-		variableTable.setAutoResizeMode(0);
-		resultTable.setAutoResizeMode(0);
+		//variableTable = new JTable(8, 2);
+		//resultTable = new JTable(8, 10);
+		//variableTable.setAutoResizeMode(0);
+		//resultTable.setAutoResizeMode(0);
 		//to actually create the tables, remove these and just 
 		//use the generateTables() method
 		
+		generateTables();
 		variablePane = new JScrollPane(variableTable);
 		resultPane = new JScrollPane(resultTable);
 		menubar = new JMenuBar();
@@ -71,23 +75,111 @@ public class TruthGUI implements ActionListener, Runnable
 		 * this method should be used to generate the "variables"
 		 * and "results" tables and update the display to show them
 		 */
-		variableTable.setAutoResizeMode(0);
-		resultTable.setAutoResizeMode(0);
+		int columns = variables;
+		int rows = (int)Math.pow(2, variables);
+		
+		//setup the variable table
+		variableTable = new JTable(rows, columns);
+		for (int c = columns - 1; c >= 0; c--)
+			for (int r = 0; r < rows; r++)
+				variableTable.setValueAt((r/(int)Math.pow(2, c))%2, r, c);
+		
+		//setup the result table
+		resultTable = generateResultTable();
+		
+		//make sure columns are formatted correctly
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		variableTable.setDefaultRenderer(Object.class, centerRenderer);
+		resultTable.setDefaultRenderer(Object.class, centerRenderer);
+		
+		//refresh tables
+		variableTable.setRowSelectionAllowed(false);
+		resultTable.setRowSelectionAllowed(false);
+		//variableTable.setAutoResizeMode(0);
+		//resultTable.setAutoResizeMode(0);
 	}
 	
+	public JTable generateResultTable()
+	{
+		String[] columnNames = {"AND", "OR", "NOR", "XOR","NAND"};
+		Object[][] rowData = null;
+		
+		// determines what column names and rowData we are using based on # of vars
+		if (variables == 2)
+		{			
+			Object[][] data =
+				{
+						 {0, 0, 1, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 1, 1},
+						 {1, 1, 0, 0, 0}
+				};
+			rowData = data;
+		}
+		else if (variables == 3)
+		{
+			Object[][] data =
+				{
+						 {0, 0, 1, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 0, 1},
+						 {1, 1, 0, 0, 0}
+				};
+			rowData = data;
+		}
+		else if (variables == 4)
+		{
+			Object[][] data =
+				{
+						 {0, 0, 1, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 1, 1},
+						 {0, 1, 0, 0, 1},
+						 {0, 1, 0, 0, 1},
+						 {1, 1, 0, 0, 0}
+				};
+			rowData = data;
+		}
+		
+		
+		JTable table = new JTable(rowData, columnNames);
+		return table;
+	}
+
 	public void actionPerformed(ActionEvent e)  //events for clicking items
 	{
 		Object source = e.getSource();
 		
 		if (source == addVar)
 		{
-			variables++;
-			generateTables();
+			if (variables < maxVars)
+			{
+				variables++;
+				generateTables();
+			}
 		}
 		else if (source == removeVar)
 		{
-			variables--;
-			generateTables();
+			if (variables > 0)
+			{
+				variables--;
+				generateTables();
+			}
 		}
 	}
 	
